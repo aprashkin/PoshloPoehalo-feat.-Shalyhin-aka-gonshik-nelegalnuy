25f0ab912fcfe8650dc0f146c89fd860581819af
aprAshkin <aprAshkin@DESKTOP-V1QV72U>
2023-10-02 22:33:17 +0300

    Добавьте файлы проекта.

---
+++ PoshloPoehalo feat. Shalyhin aka gonshik nelegalnuy/CarAuto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassyMetody
+{
+    internal class CarAuto
+    {
+        private string number;
+        private float fuel;
+        private float flow;
+
+        public void info(string number, float fuel, float flow)
+        {
+            this.number = number;
+            this.fuel = fuel;
+            this.flow = flow;
+        }
+        public void outinfo()
+        {
+            Console.WriteLine($"Номер: {this.number}\nТопливо: {this.fuel} \nРасход: {this.flow}");
+        }
+
+        public void zaprawka(float top)
+        {
+            this.fuel += top;
+        }
+        public void move(int km)
+        {
+
+            float ostatok = this.fuel - (km * (this.flow) / 100);
+            if (ostatok > 0)
+            {
+                Console.WriteLine($"Мы смогли доехать до кабинета, нас отметили, как пропавшими без вести, зато в баке осталось {ostatok} л.");
+            }
+            if (ostatok < 0)
+            {
+                Console.WriteLine("Не докатимся, толкать сам дальше будешь? Сколько еще литров зальем?");
+                zaprawka(float.Parse(Console.ReadLine()));
+                if (ostatok > 0)
+                {
+                    Console.WriteLine($"Мы смогли доехать до кабинета, нас отметили, как пропавшими без вести, зато в баке осталось {ostatok} л." );
+                }
+                if (ostatok < 0)
+                {
+                    float for_ostatok = (ostatok * -1);
+                    Console.WriteLine($"Нам не хватило топлива до кабинета. Нужно было залить ещё {for_ostatok:F1} литра(ов). По пути нас съел javascript...");
+                }
+            }
+
+
+        }
+    }
+}
---
+++ PoshloPoehalo feat. Shalyhin aka gonshik nelegalnuy/Program.cs
@@ -0,0 +1,12 @@
+﻿using ClassyMetody;
+
+
+CarAuto carAuto = new CarAuto();
+Console.WriteLine("Количество топлива: ");
+carAuto.info("1ААА23", float.Parse(Console.ReadLine()), 10.0F);
+carAuto.outinfo();
+Random random = new Random();
+int a = random.Next(1, 200);
+Console.WriteLine($"До кабинета 1.11 (ДАЛЬШЕ БОГА НЕТ, до него осталось {a} км)");//эту часть кода придумал не я
+carAuto.move(km: a);
+
